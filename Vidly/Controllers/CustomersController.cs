dd4d827e362173fc5950618928796749de25d6a7
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-08-27 08:10:00 +0200

    Add Customers Controller and modify App name and navigation bar

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "Customer1"},
+                new Customer() { Name = "Customer2"}
+            };
+
+            return View(customers);
+        }
+    }
+}

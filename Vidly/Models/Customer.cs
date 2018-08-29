4c9c8f70242e4ef50b525c4fc165d49f48250203
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-08-29 21:01:12 +0200

    S4-L39 Labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -14,7 +14,9 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; } // Navigation property
         public byte MembershipTypeId { get; set; } // Foreign Key

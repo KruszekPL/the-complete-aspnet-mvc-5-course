e24f80cff795851715ccfb728274b7112ec1e70a
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-08-31 12:33:26 +0200

    S5-L54 Validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -15,6 +15,7 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
